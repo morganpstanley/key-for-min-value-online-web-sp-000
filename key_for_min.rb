5a06682fdc836666ded70ffc562e45f323471270
Morgan Stanley <morganpstanley@gmail.com>
2019-07-07 11:14:49 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  current = 100
+  outcome = ""
+  if name_hash == {}
+    outcome = nil
+  else
+    name_hash.each do |key, value|
+      if value < current
+        current = value
+        outcome = key
+      end
+    end
+  end
+  outcome
 end
